b551a226e0a99689f5d834e5d56395a6d68f629a
Athul K R <athulkr34@gmail.com>
2026-06-21 20:41:31 +0530

    Time: 15 ms (19.33%) | Memory: 21.1 MB (87.66%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,10 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         freq = {}
+        
         for char in nums:
             if char in freq:
-                freq[char] +=1
+                freq[char] += 1
             else:
                 freq[char] = 1
+        
+        for num, count in freq.items():
+            if count > len(nums) // 2:
+                return num
 
-        return max(freq, key=freq.get)
+            
